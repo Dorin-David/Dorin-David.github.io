9527118f86e9317106931cb544b871b387626f37
Dorin David <70648503+Dorin-David@users.noreply.github.com>
2021-06-25 19:13:43 +0200

    add gulp pipe

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-const {src, dest} = require('gulp');
+const {src, dest, parallel} = require('gulp');
 const minImg = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
 
 function minifyImg() {
     return src('./src/assets/images/*').pipe(minImg()).pipe(dest('./public/images'))
 }
 
+function minifyCSS(){
+    return src('./src/style/*.css').pipe(cleanCSS()).pipe(dest('./public/style'))
+}
 
-exports.default =  minifyImg
+
+exports.default =  parallel(minifyImg, minifyCSS)
