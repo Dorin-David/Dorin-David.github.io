5c7f121b7b8dd168dd15395888a857a1ddc9bb2a
Dorin David <doriodavid@gmail.com>
2022-01-29 17:26:56 +0100

    delete project

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,7 +4,6 @@ const images = {
     dintorni: ['main.JPG','raccolte.JPG', 'authors.JPG','categories.JPG', 'article.JPG'],
     js_quiz: ['main.JPG', 'sample_1.JPG', 'sample_2.JPG', 'review.JPG', 'styles.JPG', 'sample_3.JPG', ],
     codewars_database: ['user_card.JPG', 'katas.JPG', 'kata_info.JPG', 'user_card_2.JPG', 'kata_info2.JPG'],
-    mondo_pane: ['main.JPG', 'pane.JPG', 'farine_main.JPG', 'farine.JPG', 'farine_small.JPG'],
     chess_database: ["main3.PNG", "loading.PNG", 'filter1.PNG', 'filter2.PNG', 'filter3.PNG']
 }
 
